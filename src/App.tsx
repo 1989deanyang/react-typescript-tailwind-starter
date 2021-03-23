8d46c2372aa107a32d78b92e25e42f84c1d48959
1989deanyang <1989deanyang@gmail.com>
2021-03-24 00:02:51 +1300

    create brand new react 17 app, with typescript 4.2 and tailwind v2

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="text-green-500 bg-blue-300">
+      <p>React typescript tailwind starter</p>
+    </div>
+  );
+}
+
+export default App;
